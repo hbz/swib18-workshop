6190330ae7162eb67d7c0b289cf49b5a3dcc5dff
Fabian Steeg <steeg@hbz-nrw.de>
2018-09-26 17:36:15 +0200

    Use context and frame in JSON-LD conversion
    Using local context in node code (`npm start`) and remote context
    on GitHub in the HTML file (open data/index.html in a browser).
    
    See https://github.com/hbz/swib18-workshop/issues/2

--- js/app.js
+++ js/app.js
@@ -14,5 +14,28 @@ fs.readFile('data/loc_bib_works_test.nt', function (err, data) {
     console.log("JSON-LD output:")
     console.log("===============")
     console.log(JSON.stringify(doc, null, 2));
+
+    var frame = {
+      "@type": "http://id.loc.gov/ontologies/bibframe/Work",
+      "@embed": "@always"
+    };
+
+    jsonld.frame(doc, frame, (err, framed) => {
+      console.log("===============")
+      console.log("JSON-LD framed:")
+      console.log("===============")
+      console.log(JSON.stringify(framed, null, 2));
+
+      fs.readFile('data/bibframe-context.jsonld', function (err, context) {
+        jsonld.compact(framed, JSON.parse(context), function(err, compacted) {
+          if(err) {console.log(err);}
+          console.log("================")
+          console.log("JSON-LD compact:")
+          console.log("================")
+          console.log(JSON.stringify(compacted, null, 2));
+        });
+      });
+
+    });
   });
 });
