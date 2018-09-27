1b6709f76ca4e8096a9873d7c538a219ae664919
Fabian Steeg <steeg@hbz-nrw.de>
2018-09-27 14:24:24 +0200

    Add final postprocessing, write to files, use data
    Remove `@graph`, add context URL, access fields as attributes
    
    See https://github.com/hbz/swib18-workshop/issues/2

--- js/app.js
+++ js/app.js
@@ -3,39 +3,51 @@ const fs = require('fs');
 
 fs.readFile('data/loc_bib_works_test.nt', function (err, data) {
   if (err) { throw err; }
-  console.log("==============")
-  console.log("Input triples:")
-  console.log("==============")
-  console.log(data.toString());
-
   jsonld.fromRDF(data.toString(), {format: 'application/n-quads'}, (err, doc) => {
     if (err) { throw err; }
-    console.log("===============")
-    console.log("JSON-LD output:")
-    console.log("===============")
-    console.log(JSON.stringify(doc, null, 2));
-
     var frame = {
       "@type": "http://id.loc.gov/ontologies/bibframe/Work",
       "@embed": "@always"
     };
-
     jsonld.frame(doc, frame, (err, framed) => {
-      console.log("===============")
-      console.log("JSON-LD framed:")
-      console.log("===============")
-      console.log(JSON.stringify(framed, null, 2));
-
+      if (err) { throw err; }
       fs.readFile('data/bibframe-context.jsonld', function (err, context) {
         jsonld.compact(framed, JSON.parse(context), function(err, compacted) {
           if(err) {console.log(err);}
-          console.log("================")
-          console.log("JSON-LD compact:")
-          console.log("================")
-          console.log(JSON.stringify(compacted, null, 2));
+          var final = postprocess(compacted);
+          console.log('=== Works: ===')
+          for(i in final) {
+            console.log(final[i].contribution[0].agent[0].label + ': ' + final[i].label);
+          }
+          console.log('==============')
+          write('data/loc_bib_works_test-1_output.json', doc);
+          write('data/loc_bib_works_test-2_framed.json', framed);
+          write('data/loc_bib_works_test-3_compact.json', compacted);
+          write('data/loc_bib_works_test-4_final.json', final);
         });
       });
-
     });
   });
 });
+
+function write(location, content) {
+  fs.writeFile(location, JSON.stringify(content, null, 2), (err) => {
+    if (err) throw err;
+    console.log('Wrote: ' + location);
+  });
+}
+
+function postprocess(content) {
+  var docs = content['@graph'];
+  var clean = [];
+  for(i in docs) {
+    var cleanDoc = {
+      '@context': 'https://raw.githubusercontent.com/hbz/swib18-workshop/2-convert/data/bibframe-context.jsonld'
+    };
+    for(x in docs[i]) {
+      cleanDoc[x] = docs[i][x];
+    }
+    clean.push(cleanDoc);
+  }
+  return clean;
+}
